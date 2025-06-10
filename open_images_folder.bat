662138613c1299bd4a11ac4e1e8b4326f9244287
Nathan Obiri <nathan.obiri@example.com>
2025-06-10 23:13:30 +0300

    Enhance Nairobi article with new images and styling, update homepage design

---
+++ open_images_folder.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Opening Nairobi images folder...
+start "" "C:\Users\HP\CascadeProjects\nathan_obiri_portfolio_v2\articles\images\nairobi"
+echo If the folder didn't open, please go to:
+echo C:\Users\HP\CascadeProjects\nathan_obiri_portfolio_v2\articles\images\nairobi
+pause
